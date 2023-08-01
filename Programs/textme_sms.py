198d3378ada5f0e0e462b6ce95cfaf60995f6dc4
Rohan-2 <71371358+Rohan-2@users.noreply.github.com>
2023-08-02 02:17:34 +0530

    Add files via upload

---
+++ Programs/textme_sms.py
@@ -0,0 +1,13 @@
+from twilio.rest import Client
+
+account_sid = "get from twilio trail user dashboard"
+auth_token = "get from twilio trail user dashboard"
+client = Client(account_sid, auth_token)
+
+message = client.message.creat(
+    from_ = "twilio generated number"
+    body = "content to be sent"
+    to = "number to which sms has to be sent"
+)
+
+print(message.sid)
