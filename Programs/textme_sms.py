9f071b48d1b7a75604a05ae97cd613a735184980
Rohan-2 <71371358+Rohan-2@users.noreply.github.com>
2023-08-02 02:27:09 +0530

    Update textme_sms.py

--- Programs/textme_sms.py
+++ Programs/textme_sms.py
@@ -4,7 +4,7 @@ account_sid = "get from twilio trail user dashboard"
 auth_token = "get from twilio trail user dashboard"
 client = Client(account_sid, auth_token)
 
-message = client.message.creat(
+message = client.message.create(
     from_ = "twilio generated number"
     body = "content to be sent"
     to = "number to which sms has to be sent"
